b30507fbc70c09a4cf0bb3e3ba0cf69afb9768fe
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-05-13 10:37:21 -0400

    Add a basic CLI, improve library

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright 2021 Sambhav Saggi
+// Licensed under the MIT license (see LICENSE or https://opensource.org/licenses/MIT)
+
+// TODO: Docs
+
+pub mod parser {
+    pub fn parse(config: &String) {} // Parse compressed game config file
+    pub fn convert(text: &String) -> String {
+        return text.clone();
+    } // Convert text file into compressed game config file
 }
+
+mod player {
+    fn initialize() {} // Load game assets
+    fn start() {} // Starting point for the game
+    fn help() {} // Return help
+    fn choose() {} // Choose option from step, return next step
+    fn get_step() {} // Return text for step
+    fn credits() {} // Return credits for game
+}
+
+#[cfg(test)]
+mod tests {} // TODO: Tests
