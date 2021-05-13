3538c7c9e55ea30a99bda57c2b2aeda43fd024b4
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-05-13 12:08:42 -0400

    Add more framework, sample config

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,59 @@
 
 // TODO: Docs
 
-pub mod parser {
-    pub fn parse(config: &String) {} // Parse compressed game config file
-    pub fn convert(text: &String) -> String {
-        return text.clone();
-    } // Convert text file into compressed game config file
-}
+mod game {
+    struct Config {
+        title: String,
+        author: String,
+        minwidth: u16,
+        minheight: u16,
+    }
+    
+    impl Config {
+        pub fn getAuthor() -> str {
+            
+        }
+    }
 
-mod player {
-    fn initialize() {} // Load game assets
-    fn start() {} // Starting point for the game
-    fn help() {} // Return help
-    fn choose() {} // Choose option from step, return next step
-    fn get_step() {} // Return text for step
-    fn credits() {} // Return credits for game
-}
+    struct PathOption {
+        key: u32,
+        data: String,
+        pathkey: u32,
+    }
 
-#[cfg(test)]
-mod tests {} // TODO: Tests
+    struct Path {
+        key: u32,
+        data: String,
+        options: Vec<PathOption>,
+    }
+
+    struct State {
+        position: u32,
+        history: String,
+    }
+
+    pub mod parser {
+        pub fn parse(config: &String) {} // Parse compressed game config file
+
+        // Convert text file into compressed game config file
+        pub fn convert(text: &String) -> String {
+            for line in text.lines() {
+                // a
+            }
+            "aaa".to_string()
+        }
+    }
+
+    mod player {
+        fn initialize(config: &String) {} // Load game assets
+        fn start() {} // Starting point for the game
+        fn help() {} // Return help
+        fn choose() {} // Choose option from step, return next step
+        fn get_step() {} // Return text for step
+        fn load(posititon: &String) {} // Load position
+        fn credits() {} // Return credits for game
+    }
+
+    #[cfg(test)]
+    mod tests {} // TODO: Tests
+}
